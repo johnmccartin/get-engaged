98ce0666000bb0a787b6036756fca3b02957a15e
John McCartin <john.mccartin@gmail.com>
2016-12-07 10:50:45 -0500

    Install formbuilder.

--- admin/js/main.js
+++ admin/js/main.js
@@ -3,30 +3,10 @@ var fire = {};
 $(document).ready(function(){
 	setSizes();
 
+	$('.build-wrap').formBuilder();
 
+	
 
-	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	    maxZoom: 18,
-	    id: 'mapbox.satellite',
-	    accessToken: authy.mb
-	}).addTo(mymap);
-
-	var marker;
-
-	mymap.on('click',function(e){
-		if(marker) {
-			mymap.removeLayer(marker);
-		}
-		marker = new L.marker(e.latlng,{draggable:false});
-		mymap.addLayer(marker);
-		fire.latlng = e.latlng;
-		fire.lat = e.latlng.lat;
-		fire.lng = e.latlng.lat;
-	})
-
-	//$('.content img').addClass('pure-img');
 
 
 	$(window).load(function(){
